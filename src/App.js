6754f6d43cca8aa3c300ad008105c7371a427521
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 08:43:16 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+import { Component } from "react";
 
-function App() {
-  return (
-    <>
-      <ValidationSample />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </>
+    );
+  }
 }
 
 export default App;
