5d7a789bbfdd5eeb5948544d73e5d9a6a1cfd936
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 08:57:19 +0900

    component repetition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import ScrollBox from "./ScrollBox";
 import { Component } from "react";
+import IterationSapmle from "./IterationSample";
 
 class App extends Component {
   render() {
     return (
       <>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <IterationSapmle />
       </>
     );
   }
