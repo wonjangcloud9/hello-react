5cfe18281738f24e99b94d3bcdc33a33fe99289e
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 09:38:34 +0900

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { Component } from "react";
-import IterationSapmle from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <>
-        <IterationSapmle />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </>
     );
   }
